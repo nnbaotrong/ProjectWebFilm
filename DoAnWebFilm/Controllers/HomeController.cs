58b47a477a45f69bd396231e013759d39c11ca5a
Nguyen Ngoc Bao Trong <nnbaotrong@gmail.com>
2021-11-20 22:47:56 +0700

    update nhiu chuc nang

--- DoAnWebFilm/Controllers/HomeController.cs
+++ DoAnWebFilm/Controllers/HomeController.cs
@@ -16,12 +16,14 @@ namespace DoAnWebFilm.Controllers
             return db.Phims.OrderByDescending(a => a.id_phim).Take(count).ToList();
         }
 
+        //Index
         public ActionResult Index()
         {
             var phimMoi = layPhimMoi(12);
             return View(phimMoi);
         }
 
+        //Details
         public ActionResult Details(int id)
         {
             var phim = from p in db.Phims
@@ -29,19 +31,49 @@ namespace DoAnWebFilm.Controllers
                        select p;
             return View(phim.Single());
         }
-
-        public ActionResult About()
+        //Watch Movie
+        public ActionResult WatchMovie(int id)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var phim = from p in db.Phims
+                       where p.id_phim == id
+                       select p;
+            return View(phim.Single());
         }
 
-        public ActionResult Contact()
+        //Type Movie
+        public ActionResult TypeMovie()
         {
-            ViewBag.Message = "Your contact page.";
+            var type = from t in db.Loais select t;
+            return PartialView(type);
+        }
+        public ActionResult MovieForType(int id)
+        {
+            var movie = from m in db.Phims where m.id_phim == id select m;
+            return View(movie);
+        }
 
-            return View();
+        //Country
+        public ActionResult Country()
+        {
+            var country = from c in db.QuocGias select c;
+            return PartialView(country);
+        }
+        public ActionResult MovieForCountry(int id)
+        {
+            var movie = from m in db.Phims where m.id_quoc_gia == id select m;
+            return View(movie);
+        }
+
+        //Release year
+        public ActionResult ReleaseYear()
+        {
+            var releaseYear = from ry in db.NamPhatHanhs select ry;
+            return PartialView(releaseYear);
+        }
+        public ActionResult MovieForReleaseYear(int id)
+        {
+            var movie = from m in db.Phims where m.id_nam == id select m;
+            return View(movie);
         }
     }
 }
